783b451bf3130306180abfc13cbdc41dafb5398c
muaz <ultimabytes@gmail.com>
2023-05-26 16:05:48 +0500

    UI added

---
+++ app/src/main/java/com/ultimabyte/bpoultry/loginActivity.java
@@ -0,0 +1,14 @@
+package com.ultimabyte.bpoultry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
